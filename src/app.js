46ca3bb97d0ce3951911d157f7c0d722ccb87be4
Kaiena <kaienafmartins@gmail.com>
2023-01-05 16:27:26 -0300

    create tweets and sign-up pages

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ import express  from "express";
 
 const server = express();
 
+server.get("/tweets", (req, res) => {
+  res.send("Olá")
+});
+
+server.get("/sign-up", (req, res) => {
+  res.send("Cadastro")
+});
+
 server.listen(5000, () => {
   console.log("Servidor funcionou")
 })
