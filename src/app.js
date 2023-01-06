5a94cbfaa3ed1a38fb4ed1510144711337a1ee1c
Kaiena <kaienafmartins@gmail.com>
2023-01-06 11:39:45 -0300

    create sign-up route

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 import express  from "express";
 
 const server = express();
+server.use(express.json());
+const users = [];
 
 server.get("/tweets", (req, res) => {
   res.send("Olá")
 });
 
-server.get("/sign-up", (req, res) => {
-  res.send("Cadastro")
+server.post("/sign-up", (req, res) => {
+  const {username, avatar} = req.body;
+  users.push({username, avatar});
+  res.send("OK");
 });
+
 
 server.listen(5000, () => {
   console.log("Servidor funcionou")
